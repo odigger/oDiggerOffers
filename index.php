c3a76b6017c47003e3002ed25b0c610c891c2b9a
unknown <galz@.(none)>
2012-01-16 10:58:38 -0500

    added background to search form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <? include_once('oDiggerOffers-config.php'); ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<title>oDigger Offers</title>
@@ -16,8 +17,7 @@
 	<form id="offers_form" class="offers_form" onsubmit="return false;">
 		<table class="offers_form_table" cellspacing='0' cellpadding='5'>
 			<tr>
-				<th>Search:</th>
-				<td><input class="query" type="text" name="query" value="<? echo htmlentities($_SESSION['oDiggerOffers_query']); ?>"></td>
+				<td class="offers_search_box"><input class="query" type="text" name="query" value="<? echo htmlentities($_SESSION['oDiggerOffers_query']); ?>"></td>
 	            
 				<td><? include_once('oDiggerOffers-selectNetworks.php'); ?></td>
 			
